9a4e3aa071586b623a653ae4487f378140da2183
sister <m18863010215@163.com>
2019-02-17 17:39:22 +0800

    删除无用代码

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <div id="app">
-<!-- 1 -->
-    <router-view/>
-  </div>
 </template>
 
 <script>
@@ -12,12 +8,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
